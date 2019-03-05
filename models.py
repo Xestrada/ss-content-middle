ab3d7dbbbb29aa88a1fcac26b920fccb8c6a75a9
Joshua Chen <joshuachen1@b01-aruba-authenticated-10-110-224-49.wlan.cpp.edu>
2019-03-05 10:08:10 -0800

    Removed MovieList Column from Actors Table. Updated Actor model to compensate for changes

--- models.py
+++ models.py
@@ -8,13 +8,11 @@ class Actor(db.Model):
     full_name = db.Column(db.VARCHAR)
     first_name = db.Column(db.VARCHAR)
     last_name = db.Column(db.VARCHAR)
-    movie_list = db.Column(db.VARCHAR)
 
-    def __init__(self, full_name, first_name, last_name, movie_list):
+    def __init__(self, full_name, first_name, last_name):
         self.full_name = full_name
         self.first_name = first_name
         self.last_name = last_name
-        self.movie_list = movie_list
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
@@ -25,5 +23,4 @@ class Actor(db.Model):
             'full_name': self.full_name,
             'first_name': self.first_name,
             'last_name': self.last_name,
-            'movie_list': self.movie_list,
         }
