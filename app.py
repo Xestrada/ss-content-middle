e8f3fc469c35f7d84180970484fb7ad00d2459df
Jorge Estrada <Xestrada.Jorge@gmail.com>
2019-03-10 18:50:53 -0700

    Exposed pagination headers

--- app.py
+++ app.py
@@ -619,6 +619,7 @@ def paginated_json(json_name: str, queried_results: [], page: int):
     json = make_response(jsonify({json_name: [result.serialize() for result in results]}))
     json.headers['current_page'] = page
     json.headers['max_pages'] = num_pages
+    json.headers['Access-Control-Expose-Headers'] = 'current_page, max_pages'
     return json
 
 
